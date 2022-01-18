bd271db40dc48cb7287a4f6e12cf138c3e92a80d
PriyankanDas <87163814+PriyankanDas@users.noreply.github.com>
2022-01-19 00:02:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function planet(planet) {
+    if (planet == "mercury") {
+        window.alert("Mercury is the smallest planet in our solar system. It's a little bigger than Earth's Moon. It is the closest planet to the Sun, but it's actually not the hottest.")
+    } else if (planet == "venus") {
+        window.alert("Venus is the second planet from the Sun and Earth's closest planetary neighbor. Even though Mercury is closer to the Sun, Venus is the hottest planet in our solar system. Its thick atmosphere is full of the greenhouse gas carbon dioxide, and it has clouds of sulfuric acid.")
+    } else if (planet == "earth") {
+        window.alert("Earth is an ellipsoid with a circumference of about 40,000 km. It is the densest planet in the Solar System. Of the four rocky planets, it is the largest and most massive. Earth is about eight light minutes away from the Sun and orbits it, taking a year (about 365.25 days) to complete one revolution.")
+    } else if (planet == "mars") {
+        window.alert("Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the 'Red Planet'.")
+    } else if (planet == "jupiter") {
+        window.alert("Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than two and a half times that of all the other planets in the Solar System combined, but slightly less than one-thousandth the mass of the Sun.")
+    } else if (planet == "saturn") {
+        window.alert("Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth. It only has one-eighth the average density of Earth; however, with its larger volume, Saturn is over 95 times more massive.")
+    } else if (planet == "uranus") {
+        window.alert("Uranus is the seventh planet from the Sun, and has the third-largest diameter in our solar system. It was the first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star.")
+    } else if (planet == "neptune") {
+        window.alert("Neptune is the eighth and farthest-known Solar planet from the Sun. In the Solar System, it is the fourth-largest planet by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, slightly more massive than its near-twin Uranus.")
+    }
+}
